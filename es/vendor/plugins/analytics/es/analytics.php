ee42ae17015cfbdb4e6faf1e536cf7fdc2a29bd0
duncan <duncanyu8907@gmail.com>
2023-07-16 15:13:33 +0800

    initial release

---
+++ es/vendor/plugins/analytics/es/analytics.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'sessions' => 'Sesiones',
+    'visitors' => 'Visitantes',
+    'pageviews' => 'Páginas vistas',
+    'bounce_rate' => 'Rebote',
+    'page_session' => 'Páginas/Sesión',
+    'avg_duration' => 'Duración',
+    'percent_new_session' => 'Sesiones nuevas',
+    'new_users' => 'Visitas nuevas',
+    'visits' => 'visitas',
+    'views' => 'Vistas',
+    'view_id_not_specified' => 'Debe proporcionar una identificación  válida.',
+    'credential_is_not_valid' => 'Las credenciales de Analytics no son válidas.',
+    'start_date_can_not_before_end_date' => 'La fecha de inicio :start_date cannot no puede ser posterior a la fecha de finalización :end_date',
+    'wrong_configuration' => 'Para ver las estadísticas, deberá obtener una ID en Google Analytics y agregarla a su configuración.  <br /> También necesita datos de credenciales JSON. <br />',
+    'settings' => [
+        'title' => 'Google Analytics',
+        'description' => 'Configuración para Google Analytics',
+        'tracking_code' => 'Tracking ID',
+        'tracking_code_placeholder' => 'Ejemplo: GA-12586526-8',
+        'view_id' => 'ID',
+        'view_id_description' => 'ID de Google Analytics',
+        'json_credential' => 'Credenciales de la cuenta',
+        'json_credential_description' => 'Credenciales de la cuenta',
+    ],
+    'widget_analytics_page' => 'Lo más visto',
+    'widget_analytics_browser' => 'Navegadores',
+    'widget_analytics_referrer' => 'Referencias',
+    'widget_analytics_general' => 'Análisis de la tienda',
+];
